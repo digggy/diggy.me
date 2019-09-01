919db03ac76ad451d199216c0eba3b0e390dee79
Digdarshan Kunwar <digdarshan.kunwar@gmail.com>
2019-09-01 20:58:48 +0200

    :fire: Started working on firebase

--- build/js/init.js
+++ build/js/init.js
@@ -9,12 +9,12 @@ jQuery(document).ready(function($) {
   /* FitText Settings
 ------------------------------------------------------ */
 
-  setTimeout(function() {
-    $("h1.responsive-headline").fitText(1, {
-      minFontSize: "40px",
-      maxFontSize: "70px"
-    });
-  }, 100);
+  // setTimeout(function() {
+  //   $("h1.responsive-headline").fitText(1, {
+  //     minFontSize: "40px",
+  //     maxFontSize: "70px"
+  //   });
+  // }, 100);
 
   /*----------------------------------------------------*/
   /* Smooth Scrolling
@@ -131,44 +131,44 @@ jQuery(document).ready(function($) {
   /*	contact form
 ------------------------------------------------------*/
 
-  $("form#contactForm button.submit").click(function() {
-    $("#image-loader").fadeIn();
+  // $("form#contactForm button.submit").click(function() {
+  //   $("#image-loader").fadeIn();
 
-    var contactName = $("#contactForm #contactName").val();
-    var contactEmail = $("#contactForm #contactEmail").val();
-    var contactSubject = $("#contactForm #contactSubject").val();
-    var contactMessage = $("#contactForm #contactMessage").val();
+  //   var contactName = $("#contactForm #contactName").val();
+  //   var contactEmail = $("#contactForm #contactEmail").val();
+  //   var contactSubject = $("#contactForm #contactSubject").val();
+  //   var contactMessage = $("#contactForm #contactMessage").val();
 
-    var data =
-      "contactName=" +
-      contactName +
-      "&contactEmail=" +
-      contactEmail +
-      "&contactSubject=" +
-      contactSubject +
-      "&contactMessage=" +
-      contactMessage;
-
-    $.ajax({
-      type: "POST",
-      url: "inc/sendEmail.php",
-      data: data,
-      success: function(msg) {
-        // Message was sent
-        if (msg == "OK") {
-          $("#image-loader").fadeOut();
-          $("#message-warning").hide();
-          $("#contactForm").fadeOut();
-          $("#message-success").fadeIn();
-        }
-        // There was an error
-        else {
-          $("#image-loader").fadeOut();
-          $("#message-warning").html(msg);
-          $("#message-warning").fadeIn();
-        }
-      }
-    });
-    return false;
-  });
+  //   var data =
+  //     "contactName=" +
+  //     contactName +
+  //     "&contactEmail=" +
+  //     contactEmail +
+  //     "&contactSubject=" +
+  //     contactSubject +
+  //     "&contactMessage=" +
+  //     contactMessage;
+  //   console.log(data);
+  //   $.ajax({
+  //     type: "POST",
+  //     url: "/inc/send-mail.php",
+  //     data: data,
+  //     success: function(msg) {
+  //       // Message was sent
+  //       if (msg == "OK") {
+  //         $("#image-loader").fadeOut();
+  //         $("#message-warning").hide();
+  //         $("#contactForm").fadeOut();
+  //         $("#message-success").fadeIn();
+  //       }
+  //       // There was an error
+  //       else {
+  //         $("#image-loader").fadeOut();
+  //         $("#message-warning").html(msg);
+  //         $("#message-warning").fadeIn();
+  //       }
+  //     }
+  //   });
+  //   return false;
+  // });
 });
